d74854d49b3c9726aba0c6e62985e8105d038687
KevinIansyah <keviniansyah04@gmail.com>
2025-10-15 13:30:51 +0700

    feat: custom inspiring

---
+++ app/Support/Inspiring.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Support;
+
+use Illuminate\Support\Collection;
+
+class Inspiring
+{
+  public static function quotes()
+  {
+    return new Collection([
+      'Judi online kesenangan fana. Keluarga dapat bencana. Bersikaplah bijaksana. Agar hidup tidak merana. - Kominfo',
+      'Pergi ke kondangan pake kebaya. Gandeng istri biar mesra. Siapa bilang judi bisa bikin kaya. Yang ada cuma bikin sengsara. - Kominfo',
+      'Niat hati mendaki gunung. Tetapi hari kelihatan mendung. Pemain judi berharap untung. Tapi malah jadi buntung. - Kominfo',
+      'Bunga mekar indah berseri. Udara sejuk amat terasa. Judi online bikin rugi. Judi online tak bikin kaya. - Kominfo',
+    ]);
+  }
+}
